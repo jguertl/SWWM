000668504afc8b53daf83fea43c2ecb27fd5ec75
Alex Kumer <alex.kumer@student.tugraz.at>
2017-04-11 11:23:22 +0200

    Add WelcomeScreen and change startView

---
+++ app/src/main/java/at/sw2017/swwm/WelcomeScreen.java
@@ -0,0 +1,24 @@
+package at.sw2017.swwm;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(WelcomeScreen.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
