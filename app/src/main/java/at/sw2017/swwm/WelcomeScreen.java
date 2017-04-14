0eb8a82274a0b3509a3257b1cbdda0ec6c184681
Alex Kumer <alex.kumer@student.tugraz.at>
2017-04-14 09:35:13 +0200

    Splash screen in fullscreen

--- app/src/main/java/at/sw2017/swwm/WelcomeScreen.java
+++ app/src/main/java/at/sw2017/swwm/WelcomeScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class WelcomeScreen extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
@@ -12,6 +13,9 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
