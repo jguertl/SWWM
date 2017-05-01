213a81a83965b1391c187ede96dbdd0a3b5a4715
Robert Wenig <r.wenig@student.tugraz.at>
2017-05-01 14:50:36 +0200

    Get Categoriers from DB

--- app/src/main/java/at/sw2017/todo4u/CategoryListActivity.java
+++ app/src/main/java/at/sw2017/todo4u/CategoryListActivity.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import at.sw2017.todo4u.R;
+import at.sw2017.todo4u.database.TaskCategoriesDataSource;
+import at.sw2017.todo4u.model.TaskCategory;
 
 public class CategoryListActivity extends AppCompatActivity {
 
@@ -17,7 +22,18 @@ public class CategoryListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_list);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, test);
+        TaskCategoriesDataSource tcds = new TaskCategoriesDataSource(this);
+        tcds.open();
+        List<TaskCategory> categories = new ArrayList<>();
+        categories = tcds.getAll();
+        tcds.close();
+
+        List<String> categoriesAsString = new ArrayList<>();
+        for (TaskCategory taskCategory : categories) {
+            categoriesAsString.add(taskCategory.getName());
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, categoriesAsString);
         category_list_view = (ListView) findViewById(R.id.category_list_view);
         category_list_view.setAdapter(adapter);
 
