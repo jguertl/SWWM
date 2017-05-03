d2b72787a981870db75654c67ec7e65eb6f33aaa
Michael Fürnschuß <mfuerni@users.noreply.github.com>
2017-05-03 20:54:31 +0200

    Database: Added some more tests

--- app/src/androidTest/java/at/sw2017/todo4u/DatabaseTest.java
+++ app/src/androidTest/java/at/sw2017/todo4u/DatabaseTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import java.util.List;
 
 import at.sw2017.todo4u.database.TaskCategoriesDataSource;
 import at.sw2017.todo4u.database.TasksDataSource;
+import at.sw2017.todo4u.database.Todo4uDbHelper;
 import at.sw2017.todo4u.model.Task;
 import at.sw2017.todo4u.model.TaskCategory;
 
@@ -31,12 +33,14 @@ public class DatabaseTest {
     }
 
     @Test
-    public void simpleInsertAndRead() {
+    public void simpleInsertAndReadNoNull() {
         String categoryName = "My Test Category";
         String taskTitle = "My First TEST TASK";
         String taskDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et tempus magna. Fusce dui odio, cursus quis nisi ac, luctus iaculis nulla. In mollis eget nibh et mattis. Morbi quis sapien quam. Nullam a ullamcorper magna. Integer pellentesque ipsum facilisis eros hendrerit posuere sed in ante. Donec sodales massa non neque vulputate, sagittis ullamcorper eros vehicula.";
         Calendar dueCal = Calendar.getInstance();
         dueCal.set(2017, 5, 10, 9, 30);
+        Calendar remindCal = Calendar.getInstance();
+        remindCal.set(2017, 5, 9, 14, 45);
         Date creationDate = new Date();
 
         TaskCategory cat = new TaskCategory(categoryName);
@@ -45,6 +49,7 @@ public class DatabaseTest {
         task.setTitle(taskTitle);
         task.setDescription(taskDescription);
         task.setDueDate(dueCal.getTime());
+        task.setReminderDate(remindCal.getTime());
         task.setCreationDate(creationDate);
         task.setState(Task.State.IN_PROGRESS);
 
@@ -67,8 +72,9 @@ public class DatabaseTest {
         assertEquals(taskTitle, dbTask.getTitle());
         assertEquals(taskDescription, dbTask.getDescription());
         assertEquals(dueCal.getTime(), dbTask.getDueDate());
+        assertEquals(remindCal.getTime(), dbTask.getReminderDate());
         assertEquals(creationDate, dbTask.getCreationDate());
-        assertNull(dbTask.getReminderDate());
+        assertEquals(remindCal.getTime(), dbTask.getReminderDate());
         assertEquals(Task.State.IN_PROGRESS, dbTask.getState());
 
         List<Task> tasksInCategory = tDs.getTasksInCategory(cat);
@@ -78,7 +84,239 @@ public class DatabaseTest {
         assertEquals(task.getId(), tasksInCategory.get(0).getId());
 
         tDs.close();
+    }
+    @Test
+    public void simpleInsertAndReadManyNull() {
+        String categoryName = "My Null Category";
+        String taskTitle = "My Null TASK";
+        String taskDescription = "Lorem ipsum dolor sit NULLPOINTER.";
 
+        TaskCategory cat = new TaskCategory(categoryName);
+        Task task = new Task();
+        task.setCategory(cat);
+        task.setTitle(taskTitle);
+        task.setDescription(taskDescription);
+
+        TaskCategoriesDataSource tcDs = new TaskCategoriesDataSource(instrumentationCtx);
+        tcDs.open();
+        assertTrue(tcDs.insertOrUpdate(cat));
+        tcDs.close();
+
+
+        TasksDataSource tDs = new TasksDataSource(instrumentationCtx);
+        tDs.open();
+        assertTrue(tDs.insertOrUpdate(task));
+
+        Task dbTask = tDs.getById(task.getId());
+        assertNotNull(dbTask);
+
+        assertEquals(task.getId(), dbTask.getId());
+        assertEquals(cat.getId(), dbTask.getCategory().getId());
+        assertEquals(categoryName, dbTask.getCategory().getName());
+        assertEquals(taskTitle, dbTask.getTitle());
+        assertEquals(taskDescription, dbTask.getDescription());
+        assertNull(dbTask.getDueDate());
+        assertNull(dbTask.getReminderDate());
+        assertNull(dbTask.getCreationDate());
+        assertNull(dbTask.getState());
+
+        List<Task> tasksInCategory = tDs.getTasksInCategory(cat);
+
+        assertNotNull(tasksInCategory);
+        assertEquals(1, tasksInCategory.size());
+        assertEquals(task.getId(), tasksInCategory.get(0).getId());
+
+        tDs.close();
+    }
+    @Test
+    public void simpleInsertAndReadDescriptionAndCategoryNull() {
+        String taskTitle = "My Null Null TASK";
+
+        Task task = new Task();
+        task.setTitle(taskTitle);
+
+        TasksDataSource tDs = new TasksDataSource(instrumentationCtx);
+        tDs.open();
+        assertTrue(tDs.insertOrUpdate(task));
+
+        Task dbTask = tDs.getById(task.getId());
+        assertNotNull(dbTask);
+
+        assertEquals(task.getId(), dbTask.getId());
+        assertNull(dbTask.getCategory());
+        assertEquals(taskTitle, dbTask.getTitle());
+        assertNull(dbTask.getDescription());
+        assertNull(dbTask.getDueDate());
+        assertNull(dbTask.getReminderDate());
+        assertNull(dbTask.getCreationDate());
+        assertNull(dbTask.getState());
+
+        tDs.close();
+    }
+
+    @Test
+    public void openCategoryDSBeforeReading() {
+        String categoryName = "My Null Category";
+        String taskTitle = "My Null TASK";
+        String taskDescription = "Lorem ipsum dolor sit NULLPOINTER.";
+
+        TaskCategory cat = new TaskCategory(categoryName);
+        Task task = new Task();
+        task.setCategory(cat);
+        task.setTitle(taskTitle);
+        task.setDescription(taskDescription);
+
+        TaskCategoriesDataSource tcDs = new TaskCategoriesDataSource(instrumentationCtx);
+        tcDs.open();
+        assertTrue(tcDs.insertOrUpdate(cat));
+
+
+        TasksDataSource tDs = new TasksDataSource(instrumentationCtx);
+        tDs.open();
+        assertTrue(tDs.insertOrUpdate(task));
+
+        Task dbTask = tDs.getById(task.getId());
+        assertNotNull(dbTask);
+
+        assertEquals(task.getId(), dbTask.getId());
+        assertEquals(cat.getId(), dbTask.getCategory().getId());
+        assertEquals(categoryName, dbTask.getCategory().getName());
+        assertEquals(taskTitle, dbTask.getTitle());
+        assertEquals(taskDescription, dbTask.getDescription());
+        assertNull(dbTask.getDueDate());
+        assertNull(dbTask.getReminderDate());
+        assertNull(dbTask.getCreationDate());
+        assertNull(dbTask.getState());
+
+        List<Task> tasksInCategory = tDs.getTasksInCategory(cat);
+
+        assertNotNull(tasksInCategory);
+        assertEquals(1, tasksInCategory.size());
+        assertEquals(task.getId(), tasksInCategory.get(0).getId());
+
+        tDs.close();
+        tcDs.close();
+    }
+
+    @Test
+    public void insertUpdateDelete() {
+        String categoryName = "My Test Category";
+        String taskTitle = "My First TEST TASK";
+        String taskDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et tempus magna. Fusce dui odio, cursus quis nisi ac, luctus iaculis nulla. In mollis eget nibh et mattis. Morbi quis sapien quam. Nullam a ullamcorper magna. Integer pellentesque ipsum facilisis eros hendrerit posuere sed in ante. Donec sodales massa non neque vulputate, sagittis ullamcorper eros vehicula.";
+        Calendar dueCal = Calendar.getInstance();
+        dueCal.set(2017, 5, 10, 9, 30);
+        Calendar remindCal = Calendar.getInstance();
+        remindCal.set(2017, 5, 9, 14, 45);
+        Date creationDate = new Date();
+
+        TaskCategory cat = new TaskCategory(categoryName);
+        Task task = new Task();
+        task.setCategory(cat);
+        task.setTitle(taskTitle);
+        task.setDescription(taskDescription);
+
+        TaskCategoriesDataSource tcDs = new TaskCategoriesDataSource(instrumentationCtx);
+        tcDs.open();
+        assertTrue(tcDs.insertOrUpdate(cat));
+        tcDs.close();
+
+
+        TasksDataSource tDs = new TasksDataSource(instrumentationCtx);
+        tDs.open();
+        assertTrue(tDs.insertOrUpdate(task));
+
+        Task dbTask = tDs.getById(task.getId());
+        assertNotNull(dbTask);
+
+        assertEquals(task.getId(), dbTask.getId());
+        assertEquals(cat.getId(), dbTask.getCategory().getId());
+        assertEquals(categoryName, dbTask.getCategory().getName());
+        assertEquals(taskTitle, dbTask.getTitle());
+        assertEquals(taskDescription, dbTask.getDescription());
+        assertNull(dbTask.getDueDate());
+        assertNull(dbTask.getReminderDate());
+        assertNull(dbTask.getCreationDate());
+        assertNull(dbTask.getState());
+
+        String taskDescription2 = "The new description.";
+        String taskTitle2 = "The new title.";
+
+        dbTask.setDescription(taskDescription2);
+        dbTask.setTitle(taskTitle2);
+        dbTask.setDueDate(dueCal.getTime());
+        dbTask.setReminderDate(remindCal.getTime());
+        dbTask.setCreationDate(creationDate);
+        dbTask.setState(Task.State.IN_PROGRESS);
+
+        tDs.insertOrUpdate(dbTask);
+
+        dbTask = tDs.getById(task.getId());
+        assertNotNull(dbTask);
+
+        assertEquals(task.getId(), dbTask.getId());
+        assertEquals(cat.getId(), dbTask.getCategory().getId());
+        assertEquals(categoryName, dbTask.getCategory().getName());
+        assertEquals(taskTitle2, dbTask.getTitle());
+        assertEquals(taskDescription2, dbTask.getDescription());
+        assertEquals(dueCal.getTime(), dbTask.getDueDate());
+        assertEquals(remindCal.getTime(), dbTask.getReminderDate());
+        assertEquals(creationDate, dbTask.getCreationDate());
+        assertEquals(remindCal.getTime(), dbTask.getReminderDate());
+        assertEquals(Task.State.IN_PROGRESS, dbTask.getState());
+
+        List<Task> tasksInCategory = tDs.getTasksInCategory(cat);
+
+        assertNotNull(tasksInCategory);
+        assertEquals(1, tasksInCategory.size());
+        assertEquals(task.getId(), tasksInCategory.get(0).getId());
+
+        assertTrue(tDs.delete(dbTask));
+        assertFalse(tDs.delete(task.getId()));
+
+        tDs.close();
+    }
+
+    @Test
+    public void getAllTasks() {
+        Todo4uDbHelper helper = new Todo4uDbHelper(instrumentationCtx);
+        helper.onDowngrade(helper.getWritableDatabase(), 1, 0);
+        helper.close();
+
+        TasksDataSource tDs = new TasksDataSource(instrumentationCtx);
+        tDs.open();
+        assertEquals(0, tDs.getAll().size());
+        tDs.close();
+
+
+        TaskCategory tc = new TaskCategory("My Cat");
+        TaskCategoriesDataSource tcDs = new TaskCategoriesDataSource(instrumentationCtx);
+        assertFalse(tcDs.isDatabaseOpen());
+        assertFalse(tcDs.isDatabaseWriteable());
+        tcDs.open();
+        assertTrue(tcDs.isDatabaseOpen());
+        assertTrue(tcDs.isDatabaseWriteable());
+        assertTrue(tcDs.insertOrUpdate(tc));
+        assertNotEquals(0, tc.getId());
+        tcDs.close();
+
+        Task t1 = new Task("T1");
+        t1.setCategory(tc);
+
+        Task t2 = new Task("T2");
+        t2.setCategory(tc);
+
+        tDs.open();
+        assertTrue(tDs.isDatabaseWriteable());
+
+        assertTrue(tDs.insertOrUpdate(t1));
+        assertTrue(tDs.insertOrUpdate(t2));
+
+        List<Task> allTasks = tDs.getAll();
+        tDs.close();
+        assertNotNull(allTasks);
+        assertEquals(2, allTasks.size());
+        assertEquals(t1, allTasks.get(0));
+        assertEquals(t2, allTasks.get(1));
 
     }
 }
